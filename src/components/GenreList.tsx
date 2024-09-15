0167a52598c66bc100106230ee6c101c345daca3
Segun <dasamuelbb@gmail.com>
2024-09-15 20:16:47 +0100

    Show a GenreSkeleton

--- src/components/GenreList.tsx
+++ src/components/GenreList.tsx
@@ -1,17 +1,26 @@
 import { Button, List, ListItem, Text } from "@chakra-ui/react";
 import useGenre from "../hooks/useGenre";
+import GenreListSkeleton from "./GenreListSkeleton";
+import _ from "lodash";
 
 const GenreList = () => {
-  const { genres } = useGenre();
-
+  const { genres, isLoading, error } = useGenre();
+  const skeletons = _.range(50);
+  if (error) return null;
   return (
-    <List>
-      {genres.map((genre) => (
-        <Text fontSize="large" key={genre.genre_id}>
-          {genre.genre_name}
-        </Text>
-      ))}
-    </List>
+    <>
+      {isLoading &&
+        skeletons.map((skeleton) => (
+          <Text>{<GenreListSkeleton key={skeleton} />}</Text>
+        ))}
+      <List>
+        {genres.map((genre) => (
+          <Text fontSize="large" key={genre.genre_id}>
+            {genre.genre_name}
+          </Text>
+        ))}
+      </List>
+    </>
   );
 };
 
---
+++ src/components/GenreListSkeleton.tsx
@@ -0,0 +1,11 @@
+import { Skeleton, SkeletonText } from "@chakra-ui/react";
+
+const GenreListSkeleton = () => {
+  return (
+    <Skeleton>
+      <SkeletonText />
+    </Skeleton>
+  );
+};
+
+export default GenreListSkeleton;
